89102976946c6a9a49f7ad05f3687ff0e9b67d48
Ganbayar <ganabata87@gmail.com>
2024-02-08 12:50:08 +0900

    Delete split_name_path.py

--- split_name_path.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-
-def f(img_path):
-    """Given a full path to a video, return its parts."""
-    parts = img_path.split('\\')
-    filename = parts[len(parts)-1]
-    
-
-    return filename, parts[len(parts)-2]
